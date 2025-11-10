d83485a33bafc996807dbfbeb0d423abe686d072
mickeas_willams <128330949+mickeas@users.noreply.github.com>
2025-11-10 11:17:37 -0300

    Add files via upload

---
+++ assert/script/script-form.js
@@ -0,0 +1,22 @@
+
+const btnLogin = document.getElementById('btn-login');
+const btnCadastro = document.getElementById('btn-cadastro');
+const container = document.querySelector('.container');
+const cadastroForm = document.querySelector('.cadastro-form');
+const loginForm = document.querySelector('.login-form');
+
+btnLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    container.classList.add('login-ativo');
+    container.classList.remove('cadastro-ativo');
+    cadastroForm.classList.remove('active');
+    loginForm.classList.add('active');
+});
+
+btnCadastro.addEventListener('click', (e) => {
+    e.preventDefault();
+    container.classList.add('cadastro-ativo');
+    container.classList.remove('login-ativo');
+    loginForm.classList.remove('active');
+    cadastroForm.classList.add('active');
+});
