6e3426b9f4c5254532196fc9cbf6ba867cb4a9ca
larythay <laryssaath@gmail.com>
2025-12-04 01:18:45 -0300

    feat: adiciona lógica para definir a categoria 'Esporte' na URL da página de exploração

---
+++ static/js/explorar-esporte.js
@@ -0,0 +1,9 @@
+(function(){
+  const cat = 'Esporte';
+  window.__fixedCategory = cat;
+  const u = new URL(window.location.href);
+  if (!u.searchParams.get('category')) {
+    u.searchParams.set('category', cat);
+    window.history.replaceState({}, '', u.pathname + '?' + u.searchParams.toString());
+  }
+})();
